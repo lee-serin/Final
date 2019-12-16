1d3e185418fe10a3645bc6bdec8dbaf7c43b8c4e
Serin Lee <serin.lee777@gmail.com>
2019-12-16 09:35:02 -0700

    first commit

--- final.js
+++ final.js
@@ -1,34 +1,26 @@
 function ready(){
-    // (document.forms.america).onchange = function (){
-    //     console.log(document.forms.america)
-    //     console.log(this.value);
-    //     let task = document.forms.america;
-    //     let elem = document.createElement('li');
-    //     elem.innerHTML = task;
-    //     elem.append(list);
-    //     for (let t of task) {
-    //         console.log(t)
-    //         if (t.selected) {
-    //             console.log(t.value)
-    //         }
-    //     }
-
-    // }
     var checkbox = document.querySelectorAll("input[type=checkbox]");
     console.log(checkbox)
     checkbox.forEach(element => {
+
+        
         element.addEventListener( 'change', function() {
             if(this.checked) {
                 console.log(this.value)
                 let elem = document.createElement('li');
                 elem.innerHTML = this.value;
                 list.append(elem);
+
+                control.onclick(() => {
+                    str.strike        
+                });
             } else {
                 
             }
+        
         });
     });
-
-}
+    
+};
 
 document.addEventListener("DOMContentLoaded", ready);
